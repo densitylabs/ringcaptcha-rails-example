ce95afb32ee431b2be7f35bcd14ae752ab20460f
Federico Ramallo <framallo@gmail.com>
2016-08-31 13:17:07 -0500

    refactor and fix check RC button

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,18 +1,15 @@
 $(function() {
     $('#btnSubmit').click(function() {
-        var val2 = $("#ringcaptcha_pin_code").val();
-        if (val2.length == 0) {
-            alert("Please enter pin code");
-        } else if (val2.length < 4) {
-            alert("Please enter 4 digit pin code");
-
-        } else {
-            $.ajax({
-                type: 'post',
-                url: '/homes/valid_rc',
-                data: 'param1=' + $("#ringcaptcha_pin_code").val() + '&params2=' + $("#ringcaptcha_session_id").val(),
-                success: function(data) {}
-            });
-        }
+        var pinCode = $("input[name='ringcaptcha_pin_code']").first().val();
+        var sessionId = $("input[name='ringcaptcha_session_id']").first().val();
+        $.ajax({
+          type: 'post',
+          url: '/homes/valid_rc',
+          data: 'param1=' + pinCode + '&params2=' + sessionId,
+          success: function(data) {
+            console.log(data);
+            $('#result').html(data.toString())
+          }
+        });
     });
 });
