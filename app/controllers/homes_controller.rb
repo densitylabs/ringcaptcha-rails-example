60884f696b4512edfd43f1fd75e647078e6fe33c
Federico Ramallo <framallo@gmail.com>
2016-08-31 11:00:57 -0500

    Initial commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,18 @@
+class HomesController < ApplicationController
+  require 'ringcaptcha'
+  skip_before_filter :verify_authenticity_token
+
+  def index
+  end
+
+  # method to call ringcaptcha methods.
+  def valid_rc
+    rcap = RingCaptcha::RingCaptcha.new(ENV['RING_CAPTCHA_APP_KEY'], ENV['RING_CAPTCHA_SECRET_KEY'])
+    @rcap_valid = rcap.is_valid?(params[:param1], params[:params2])
+    respond_to do |format|
+      format.js
+    end
+
+  end
+
+end
